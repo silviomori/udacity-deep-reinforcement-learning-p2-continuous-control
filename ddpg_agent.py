41635634ea102cd9e26a2ad9f62dc106ae194b69
Silvio Mori Neto <silvio@technomori.com>
2019-10-25 10:55:47 -0300

    feat: Write the learning function

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -1,3 +1,4 @@
+import copy
 import random
 from collections import namedtuple, deque
 
@@ -10,8 +11,10 @@ from model import Actor, Critic
 
 _batch_size = 64
 _buffer_size = int(1e5)
-_lr_actor = 0.001
-_lr_critic = 0.001
+_gamma = 0.99
+_lr_actor = 1e-4
+_lr_critic = 1e-3
+_tau = 1e-2
 _device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
@@ -35,7 +38,7 @@ class Agent():
         self.actor_local = Actor(state_size, action_size, random_seed).to(_device)
         self.actor_target = Actor(state_size, action_size, random_seed).to(_device)
         self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=_lr_actor)
-
+        
         # Critic Network
         self.critic_local = Critic(state_size, action_size, random_seed).to(_device)
         self.critic_target = Critic(state_size, action_size, random_seed).to(_device)
@@ -56,13 +59,67 @@ class Agent():
         return action
     
     def step(self, states, actions, rewards, next_states):
-        """Save experience in replay buffer"""
+        """Save experience in replay buffer, and use random sample from buffer to learn."""
         # Save experience
         self.replay_buffer.add(states, actions, rewards, next_states)
     
-    def learn(self, experiences, gamma):
-        # TODO
-        pass
+        # Learn, if enough samples are available in memory
+        if len(self.replay_buffer) > _batch_size:
+            experiences = self.replay_buffer.sample()
+            self.learn(experiences)
+
+    def learn(self, experiences):
+        """Update policy and value parameters using given batch of experience tuples.
+        Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
+        where:
+            actor_target(state) -> action
+            critic_target(state, action) -> Q-value
+
+        Params
+        ======
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s') tuples 
+        """
+        states, actions, rewards, next_states = experiences
+        
+        # ---------------------------- update critic ---------------------------- #
+        # Get predicted next-state actions from actor_target model
+        actions_next = self.actor_target(next_states)
+        # Get predicted next-state Q-Values from critic_target model
+        Q_targets_next = self.critic_target(next_states, actions_next)
+        # Compute Q targets for current states (y_i)
+        Q_targets = rewards + (_gamma * Q_targets_next)
+        # Compute critic loss
+        Q_expected = self.critic_local(states, actions)
+        critic_loss = F.mse_loss(Q_expected, Q_targets)
+        # Minimize the loss
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+        
+        # ---------------------------- update actor ---------------------------- #
+        # Compute actor loss
+        actions_pred = self.actor_local(states)
+        actor_loss = -self.critic_local(states, actions_pred).mean()
+        # Minimize the loss
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+        
+        # ----------------------- update target networks ----------------------- #
+        self.soft_update(self.critic_local, self.critic_target)
+        self.soft_update(self.actor_local, self.actor_target)
+        
+    def soft_update(self, local_model, target_model):
+        """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+
+        Params
+        ======
+            local_model: PyTorch model (weights will be copied from)
+            target_model: PyTorch model (weights will be copied to)
+        """
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(_tau*local_param.data + (1.0-_tau)*target_param.data)
 
 
 class ReplayBuffer:
@@ -89,7 +146,7 @@ class ReplayBuffer:
         experiences = random.sample(self.memory, k=_batch_size)
         
         states = torch.from_numpy(np.vstack([e.states for e in experiences if e is not None])).float().to(_device)
-        actions = torch.from_numpy(np.vstack([e.actions for e in experiences if e is not None])).long().to(_device)
+        actions = torch.from_numpy(np.vstack([e.actions for e in experiences if e is not None])).float().to(_device)
         rewards = torch.from_numpy(np.vstack([e.rewards for e in experiences if e is not None])).float().to(_device)
         next_states = torch.from_numpy(np.vstack([e.next_states for e in experiences if e is not None])).float().to(_device)
         
